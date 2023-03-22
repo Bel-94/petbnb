91ac33b89d6ade5c7a78c2c376933d0e2b119fb2
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-22 14:32:52 +0300

    art:create a view function to test if the app is working

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse, Http404
+
 
 # Create your views here.
+def Home(request):
+    return HttpResponse("Welcome to Petbnb!")

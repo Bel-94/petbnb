64e86dfcec846544c9ab139ef9a18cc7448a8ac7
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-22 14:41:16 +0300

    art:create a url path for the app to test if its working

--- petshelter/settings.py
+++ petshelter/settings.py
@@ -78,10 +78,11 @@ WSGI_APPLICATION = "petshelter.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST' : config('DB_HOST'),
+        "NAME": BASE_DIR / "db.sqlite3",
+        # 'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST' : config('DB_HOST'),
     }
 }
 

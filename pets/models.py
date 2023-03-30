2ae771b152c47c72b1552c76b0c0847d4a2504d9
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-30 10:25:48 +0300

    art:add fields in the booking and location models

--- pets/models.py
+++ pets/models.py
@@ -20,14 +20,35 @@ class ServiceProvider(models.Model):
 class Service(models.Model):
     pass
 
-class Appointment(models.Model):
-    pass
-
 class Booking(models.Model):
-    pass
+    pet_owner = models.ForeignKey(PetOwner, on_delete=models.CASCADE)
+    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_confirmed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Bookings"
+
+    def __str__(self):
+        return f"{self.pet_owner.user.username} booked {self.service.name} with {self.service_provider.name} on {self.date} at {self.start_time}"
 
 class Location(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=255)
+    area = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name_plural = "Locations"
+
+    def __str__(self):
+        return f"{self.street_address}, {self.area}, {self.county}, {self.postal_code}"
 
 class RateandReview(models.Model):
     pass

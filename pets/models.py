83f3ab4c639020a0aaab99f11675f5232ae5bbde
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-03-30 09:22:45 +0300

    art:create the list of the models needed without the data first

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User,AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_superuser = models.BooleanField(default=False),
+    is_petowner = models.BooleanField(default=False),
+    is_serviceprovider = models.BooleanField(default=False),
+
+class PetOwner(models.Model):
+    pass
+
+class Pet(models.Model):
+    pass
+
+class ServiceProvider(models.Model):
+    pass
+
+class Service(models.Model):
+    pass
+
+class Appointment(models.Model):
+    pass
+
+class Booking(models.Model):
+    pass
+
+class Location(models.Model):
+    pass
+
+class RateandReview(models.Model):
+    pass
